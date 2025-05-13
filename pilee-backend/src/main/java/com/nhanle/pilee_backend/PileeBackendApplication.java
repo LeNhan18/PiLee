3a8534b9df7492d8cfe0d850f3d4dc66e42aded9
LeThanhNhan <lnhan8132@gmail.com>
2025-05-13 21:56:31 +0700

    New project spring boot

---
+++ pilee-backend/src/main/java/com/nhanle/pilee_backend/PileeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhanle.pilee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PileeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PileeBackendApplication.class, args);
+	}
+
+}
---
+++ pilee-backend/src/test/java/com/nhanle/pilee_backend/PileeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhanle.pilee_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PileeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
